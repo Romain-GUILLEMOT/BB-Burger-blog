b14e7a4059a3c814d19b5d7ea10ba0b7e98e93c5
EnzoDECATEAUGRUE <enzo.decatheaugrue@supdevinci.fr>
2025-03-18 11:32:42 +0100

    add tailwind and logo

--- components/elements/Loading.tsx
+++ components/elements/Loading.tsx
@@ -5,10 +5,10 @@ export default function Loading() {
     return (
         <div className="w-full h-full mx-auto my-auto cursor-wait ">
             <div className="mx-auto text-center">
-                <div className="loader">
+                <div className="loader animate-spin">
                     <img
-                        src="https://assets.ozlaloc.fr/logo/logo_min_transparent.webp"
-                        className="h-6 w-6 mx-auto"
+                        src="https://assets.romain-guillemot.dev/greenlaglogo_full.webp"
+                        className="h-32 w-32 mx-auto rounded-full animate-spin"
                         alt="Logo"
                     />
                 </div>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["app/**/*.tsx", "app/**/*.ts", "app/**/*.js", "app/**/*.jsx", "components/**/*.js", "components/**/*.jsx", "components/**/*.tsx", "components/**/*.ts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
