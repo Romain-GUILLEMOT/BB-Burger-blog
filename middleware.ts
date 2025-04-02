e3db05f2755f0faa07b48306a2e322ed32d2cdef
Romain-GUILLEMOT <contact@romain-guillemot.dev>
2025-04-02 11:23:51 +0200

    Removed Herobrine

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,11 @@ import type { NextRequest } from "next/server"
 
 export async function middleware(req: NextRequest) {
     const pathname = req.nextUrl.pathname
-    if (((req.cookies.get('__Secure-authjs.session-token') || req.cookies.get('__Secure-authjs.callback-url') || (!req.cookies.get('authjs.csrf-token') || !req.cookies.get('authjs.session-token'))) && (pathname.startsWith("/admin") || pathname.startsWith("/dashboard"))) {
+    if ((!req.cookies.get('authjs.csrf-token') || !req.cookies.get('authjs.session-token')) && (pathname.startsWith("/admin") || pathname.startsWith("/dashboard"))) {
         return NextResponse.redirect(new URL("/auth/login", req.url))
     }
 
-    if (((req.cookies.get('__Secure-authjs.session-token') || req.cookies.get('__Secure-authjs.callback-url')) || (req.cookies.get('authjs.csrf-token') && req.cookies.get('authjs.session-token'))) && pathname.startsWith("/auth")) {
+    if (req.cookies.get('authjs.csrf-token') && req.cookies.get('authjs.session-token') && pathname.startsWith("/auth")) {
         return NextResponse.redirect(new URL("/", req.url))
     }
 
